ebb1d80b08901205b8ca2c414f15c4c9e1753219
clee5rrc <clee5@academic.rrc.ca>
2024-01-04 13:57:22 -0600

    Chapter 2 update

---
+++ Chapter_2/sandbox.js
@@ -0,0 +1,38 @@
+//alert('hello, world');
+// console.log(1);
+// console.log(2);
+
+// let age = 25;
+// let year = 2019;
+
+// console.log(age, year);
+// age = 30;
+// console.log(age, year);
+
+// const points = 100;
+// points = 50;
+// console.log(points);
+
+// strings
+console.log('hello');
+let email = 'abc@abc.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Rex';
+let lastName = 'Lee';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+console.log(fullName[1]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
