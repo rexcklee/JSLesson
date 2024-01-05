f48838fc0d1f2aa84b9d0e9c5fba62b5dfcdce99
clee5rrc <clee5@academic.rrc.ca>
2024-01-05 10:26:31 -0600

    Chapter 4 completed

--- Chapter_4/sandbox.js
+++ Chapter_4/sandbox.js
@@ -39,3 +39,38 @@ const calcArea = radius => 3.14 * radius**2;  //it also work if function is simp
 
 const area = calcArea(5);
 console.log(area);
+
+// callbacks function
+const myFunc = (callbackFunc) => {
+    let value = 50;
+    callbackFunc(value);
+}
+
+myFunc(value => {console.log(value)});
+
+// foreach method (take a callback function)
+let people = ['mario','luigi','ryu','shaun','chun'];
+
+people.forEach((person)=>{console.log(`We have ${person}`)});  // pass a callback function
+
+people.forEach((person, index)=>{console.log(`${index+1}. We have ${person}`)});    // can have second parameter
+
+const logPerson = (person, index) => {
+    console.log(`${index+1}. We have ${person}`);
+};
+
+people.forEach(logPerson);
+
+// get a reference to the "ul"
+const ul = document.querySelector('.people');
+
+const people2 = ['mario','luigi','ryu','shaun','chun'];
+
+let html = ``;
+
+people2.forEach(person => {
+    html += `<li style="color: purple">${person}</li>`;
+})
+
+console.log(html);
+ul.innerHTML += html;
