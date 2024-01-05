51b3c1c1dcd41690a9af50d9d3c639d531ea4924
clee5rrc <clee5@academic.rrc.ca>
2024-01-05 08:57:01 -0600

    Chapter 3 completed

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -41,3 +41,43 @@ if (password.length >= 12 && password.includes('@')){
 // break and continue
 // break - break the loop
 // continue - stop one iteration and continue the loop
+
+// switch (using strict equality)
+const grade = 'F';
+
+switch(grade){
+    case 'A':
+        console.log("You got A");
+        break;
+    case 'B':
+        console.log("You got B");
+        break;
+    case 'C':
+        console.log("You got C");
+        break;
+    case 'D':
+        console.log("You got D");
+        break;
+    case 'E':
+        console.log("You got E");
+        break;
+    default:
+        console.log("Not a valid grade");    
+}
+
+// variable & block scope ("let" and "const" has block scope control, while "var" don't have)
+let age = 30;
+
+//let age = 50; cannot be re-define a variable
+
+if(true){
+    let age = 40;   // can be re-define inside a code block, it is local and only can be access inside the block
+    console.log('inside 1st code block: ', age);
+
+    if(true){
+        let age = 50;
+        console.log('inside 2nd code block: ', age);
+    }
+}
+
+console.log('outside code block: ', age);
