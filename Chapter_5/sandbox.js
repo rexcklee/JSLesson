2fb3c6bb295c51e5927146ef6704eab6e83c975c
clee5rrc <clee5@academic.rrc.ca>
2024-01-05 11:21:40 -0600

    Chapter 5 completed

--- Chapter_5/sandbox.js
+++ Chapter_5/sandbox.js
@@ -5,9 +5,20 @@ let user = {
     age: 30,
     email: 'crystal@theninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things'],
-    login: function(){
+    blogs: [
+        {title: 'why mac & cheese rules', like: 30}, 
+        {title: '10 things', like: 50}
+    ],
+    login(){                   // function define in object is call method
         console.log('The user logged in');
+    },
+    logout(){                   // function define in object is call method
+        console.log('The user logged out');
+    },
+    logBlogs(){       // if using arrow function, "this" inside will be refer to "window" object
+        //console.log(this.blogs);
+        console.log('This user has the following blogs:');
+        this.blogs.forEach((blog) => {console.log(blog.title, blog.like);});
     }
 }
 
@@ -20,3 +31,16 @@ user.age = 35;
 console.log(user.age);
 
 user.login();
+user.logout();
+user.logBlogs();
+
+// reference values
+
+const userOne = {name: 'ryu', age: 30};
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.age = 40;  // It change age for two user
+console.log(userOne, userTwo);
+// This is not the correct way to copy array(or any other reference type)
