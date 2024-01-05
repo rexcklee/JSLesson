15d5b875d7177f9cd6a36357895923186f00a104
clee5rrc <clee5@academic.rrc.ca>
2024-01-04 23:59:10 -0600

    Chapter 3 in progress

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,5 +1,43 @@
-for (let i=0; i<5; i++){
-    console.log("in loop:", i);
+// for (let i=0; i<5; i++){
+//     console.log("in loop:", i);
+// }
+
+// console.log('loop finished');
+
+// for loop
+// const names = ['shaun', 'mario', 'luigi'];
+
+// for (let i=0; i< names.length; i++){
+//     //console.log(names[i]);
+//     let html = `<div>${names[i]}</div>`;
+//     console.log(html);
+// }
+
+// while loop
+const names = ['shaun', 'mario', 'luigi'];
+let i = 0;
+
+// while (i < 5){
+//     console.log('in loop: ', i);
+//     i++;
+// }
+
+while (i < names.length){
+    console.log(names[i]);
+    i++;
 }
 
-console.log('loop finished');
+// else if & || &&
+const password = 'passs';
+
+if (password.length >= 12 && password.includes('@')){
+    console.log('Password is mighty strong');
+} else if (password.length >= 8 || (password.includes('@') && password.length >= 5)){
+    console.log('Password is strong enough');
+} else {
+    console.log('Password is not strong enough');
+}
+
+// break and continue
+// break - break the loop
+// continue - stop one iteration and continue the loop
